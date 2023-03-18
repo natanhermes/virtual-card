7dc33f233a5517d59f3bfa2dd24168dd4d977c13
Natã Hermes <nhermesdev@gmail.com>
2023-03-18 03:25:33 -0300

    refactor: clean code

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -10,28 +10,7 @@ import { Textarea } from '../textarea/textarea';
 import { VirtualCard } from '../virtual-card';
 import { generateImage } from '@/utils/generateImage';
 import { useCreateCollaboratorMutation } from '@/graphql/generated';
-
-const linkedinRegex = /linkedin\.com\/.{2,}/;
-const githubRegex = /github\.com\/.{3,}/;
-
-const generateFormSchema = z.object({
-  name: z.string().nonempty({ message: 'Please enter your name.' }),
-  linkedinUrl: z
-    .string()
-    .url({ message: 'Please enter a valid LinkedIn URL' })
-    .refine((value) => linkedinRegex.test(value), {
-      message: 'Please enter a LinkedIn profile URL',
-    }),
-  githubUrl: z
-    .string()
-    .url({ message: 'Please enter a valid GitHub URL' })
-    .refine((value) => githubRegex.test(value), {
-      message: 'Please enter a GitHub profile URL',
-    }),
-  description: z
-    .string()
-    .min(100, { message: 'Write at least 100 characters about yourself' }),
-});
+import { generateFormSchema } from './schemas/generate-card-schema';
 
 type GenerateFormData = z.infer<typeof generateFormSchema>;
 
